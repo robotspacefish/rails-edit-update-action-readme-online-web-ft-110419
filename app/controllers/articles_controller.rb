4bd04fed75f1f8004cb613226de8d0baf01f54ff
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-25 23:17:05 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,16 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    # raise params.inspect
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    @article.save
+
+    redirect_to article_path(@article)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:index, :new, :create]
+  # get '/articles', to: 'articles#index', as: :navigate
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch '/articles/:id', to: 'articles#update'
+  get 'articles/:id', to: 'articles#show', as: :article
 end
